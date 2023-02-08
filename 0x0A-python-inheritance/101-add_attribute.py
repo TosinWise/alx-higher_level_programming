1693c6ff3a6508c6c20cf1871149dacb159010b3
TosinWise <anuoluwapo3579@gmail.com>
2023-02-07 16:44:16 -0800

    dList

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 def add_attribute(obj, name, value):
     """ Function that adds a new attribute to an object
+
     Args:
         obj: object
         name: attribute name
         value: attribute value
+
     Raises:
         TypeError: when the attribute can't be added
+
     """
 
     if not hasattr(obj, "__dict__"):
--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -9,13 +9,14 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """ Method that recieves the value property
+
         Árgs:
             name: name of the object
             value: value of the property
+
         """
 
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-
